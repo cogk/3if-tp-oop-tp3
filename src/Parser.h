c767a53c96b3fb31609c624c143d05e8a85524ae
Corentin Forler <cogk42@users.noreply.github.com>
2020-01-04 21:49:03 +0100

    doc

--- src/Parser.h
+++ src/Parser.h
@@ -47,14 +47,13 @@ public:
     // App qui lui envoie une référence à un stream correspondant au fichier
     // spécifié par l'utilisateur.
     // Cette méthode se charge de traduire le contenu du fichier texte en trajets
-    //et créer un tableau de pointeurs vers ces trajets qu'elle renvoie.
-    // de la traduction
+    // et de créer un tableau de pointeurs vers ces trajets qu'elle renvoie.
     // Contrats :
     //  - Le stream qu'elle reçoit en paramètre doit être dans un état fonctionnel
     // cette méthode ne vérifie pas son état.
-    // - La méthode renvoi une liste de pointeurs vers les trajets contenu dans
+    //  - La méthode renvoie une liste de pointeurs vers les trajets contenu dans
     // le fichier texte.
-    // - Si le fichier texte ne respecte pas la convention d'écriture établie
+    //  - Si le fichier texte ne respecte pas la convention d'écriture établie
     // qui est expliquée en détail dans le compte-rendu, elle retourne un pointeur
     // nul et affiche sur le terminal l'erreur trouvée dans le fichier.
 
@@ -69,20 +68,19 @@ public:
     // des trajets qu'elle reçoit en paramètre uniquement les trajets dont le type
     // est celui spécfié par l'utilisateur.
     // Contrats :
-    // Contrats :
     //  - La méthode ne supprime en mémoire les trajets pointés par la liste reçue
     // en paramètre que lorsqu'elle est utilisée en chargement (le booléen
     // shouldFreeMemory a pour valeur true). Lors de la sauvegarde, les membres du
     // tableau reçu pointent vers les trajets du catalogue, la méthode ne les supprime donc
     // pas de la mémoire (le booléen shouldFreeMemory a pour valeur false).
-    // - La méthode ne renvoi rien, elle modifie l'un des paramètre qu'elle reçoit.
+    //  - La méthode ne renvoie rien, elle modifie l'un des paramètre qu'elle reçoit.
 
     static void FiltreParIndex(ListOfTrips *trips, bool shouldFreeMemory, unsigned int debut, unsigned int fin);
     // Mode d'emploi :
     // Cette méthode, appelée dans la méthode menuFiltrer(ListOfTrips*, bool)
     // de la classe App qui lui envoie un pointeur vers un ListOfTrips,
     // un booléen qui indique si les trajets doivent être supprimés en cas de
-    // non correspondance du type (opération effectuée uniquement lors du chargement)
+    // non correspondance des indices (opération effectuée uniquement lors du chargement)
     // et deux entiers naturels : debut et fin indiquant les bornes de l'intervalle
     // des indices des trajets à conserver.
     // Cette méthode se charge de ne conserver dans le tableau de pointeurs vers
@@ -94,31 +92,32 @@ public:
     // shouldFreeMemory a pour valeur true). Lors de la sauvegarde, les membres du
     // tableau reçu pointent vers les trajets du catalogue, la méthode ne les supprime donc
     // pas de la mémoire (le booléen shouldFreeMemory a pour valeur false).
-    // - Les indices donnés en paramètre à cette méthode doivent être valides,
+    //  - Les indices donnés en paramètre à cette méthode doivent être valides,
     // c'est à dire compris entre 0 et l'indice du dernier élément du paramètre
     // trips, elle fait aucune vérification.
-    // - La méthode ne renvoi rien, elle modifie l'un des paramètre qu'elle reçoit.
+    //  - La méthode ne renvoie rien, elle modifie l'un des paramètre qu'elle reçoit.
 
     static void FiltreParNom(ListOfTrips *trips, bool shouldFreeMemory, const char *startCitySearch, const char *endCitySearch);
     // Mode d'emploi :
     // Cette méthode, appelée dans la méthode menuFiltrer(ListOfTrips*, bool)
     // de la classe App qui lui envoie un pointeur vers un ListOfTrips
     // un booléen qui indique si les trajets doivent être supprimés en cas de
-    // non correspondance du type (opération effectuée uniquement lors du chargement)
-    // et deux pointeurs vers des caractères constants: l'une donnant
-    //la ville de départ et l'autre la ville d'arrivée. L'un de ses deux pointeurs
-    // peut être nul signifiant que l'utilisateur ne donne pas de condition sur
-    // la ville d'arrivé ou de départ.
+    // non respect des critères (opération effectuée uniquement lors du chargement)
+    // et deux pointeurs vers des caractères constants: l'un donnant
+    // la ville de départ et l'autre la ville d'arrivée. Les chaînes en paramètre
+    // peuvent être vides, signifiant que l'utilisateur ne donne pas de condition
+    // sur la ville d'arrivé ou de départ. Donner deux chaînes vides équivaut donc
+    // à ne pas filtrer du tout.
     // Cette méthode se charge de ne conserver dans le tableau de pointeurs vers
     // des trajets qu'elle reçoit en paramètre uniquement les trajets ayant pour
-    // ville de départ ou d'arrivée celles reçues en paramètre.
+    // ville de départ et/ou d'arrivée celles reçues en paramètre.
     // Contrats :
     //  - La méthode ne supprime en mémoire les trajets pointés par la liste reçue
     // en paramètre que lorsqu'elle est utilisée en chargement (le booléen
     // shouldFreeMemory a pour valeur true). Lors de la sauvegarde, les membres du
     // tableau reçu pointent vers les trajets du catalogue, la méthode ne les supprime donc
     // pas de la mémoire (le booléen shouldFreeMemory a pour valeur false).
-    // - La méthode ne renvoi rien, elle modifie l'un des paramètre qu'elle reçoit.
+    //  - La méthode ne renvoie rien, elle modifie l'un des paramètre qu'elle reçoit.
 
     //------------------------------------------------------------------ PRIVE
 
